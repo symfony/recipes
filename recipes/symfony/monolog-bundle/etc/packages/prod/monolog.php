d76824e010849a973cfe995553e61005a3ee93ea
Fabien Potencier <fabien.potencier@gmail.com>
2017-01-20 14:41:01 -0800

    added initial set of recipes

---
+++ recipes/symfony/monolog-bundle/etc/packages/prod/monolog.php
@@ -0,0 +1,29 @@
+<?php
+
+use Symfony\Component\Config\Resource\ClassExistenceResource;
+use Symfony\Component\Console\Application;
+
+$handlers = [
+    'main' => [
+        'type' => 'fingers_crossed',
+        'action_level' => 'error',
+        'handler' => 'nested',
+    ],
+    'nested' => [
+        'type' => 'stream',
+        'path' => '%kernel.logs_dir%/%kernel.environment%.log',
+        'level' => 'debug',
+    ],
+];
+
+$container->addResource(new ClassExistenceResource(Application::class));
+if (class_exists(Application::class)) {
+    $handlers['console'] = [
+        'type' => 'console',
+        'channels' => ['!event', '!doctrine'],
+    ];
+}
+
+$container->loadFromExtension('monolog', [
+    'handlers' => $handlers,
+]);
