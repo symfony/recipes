36565deb4e004340e85fbbf985dbe5528410071f
Samuel ROZE <samuel.roze@gmail.com>
2017-06-22 10:09:39 +0100

    Remove the `require` on autoload, as it's already loaded by Behat before

--- behat/symfony2-extension/2.1/features/bootstrap/bootstrap.php
+++ behat/symfony2-extension/2.1/features/bootstrap/bootstrap.php
@@ -2,8 +2,6 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__.'/../../vendor/autoload.php';
-
 // The check is to ensure we don't use .env in production
 if (!getenv('APP_ENV')) {
     (new Dotenv())->load(__DIR__.'/../../.env');
