48b8a77f2e5efdc8996471e19769682c64bf6311
Jannik Zschiesche <jz@becklyn.com>
2018-01-11 17:56:58 +0100

    Don’t break when not using env vars

--- symfony/framework-bundle/3.3/public/index.php
+++ symfony/framework-bundle/3.3/public/index.php
@@ -7,16 +7,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
 
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-    }
-    (new Dotenv())->load(__DIR__.'/../.env');
-}
-
 $env = $_SERVER['APP_ENV'] ?? 'dev';
 $debug = $_SERVER['APP_DEBUG'] ?? ('prod' !== $env);
+
+// The check is to ensure we don't use .env in production
+if ($debug && class_exists(Dotenv::class)) {
+    (new Dotenv())->load(__DIR__.'/../.env');
+}
 
 if ($debug) {
     umask(0000);
