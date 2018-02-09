0ba4094659a7c3b7adf908dba6e01bfef5c05d42
Miguel Simões <miguelsimoes@users.noreply.github.com>
2018-02-09 09:25:23 +0000

    Allow to disable a bundle without removing the environment from the configuration
    Ensures that the Kernel only initializes bundles that have a true value associated with the the environment and not only having it defined in the configuration which makes it easier to enable/disable bundles on a certain environment without having to remove the environment from the configuration it self.
    
    At the moment if the value is true/false it will be enable and only "NULL" will not enable the bundle, however it is clearer to have a NULL/false not enabling the bundle and only true values to enable it.

--- symfony/framework-bundle/3.3/src/Kernel.php
+++ symfony/framework-bundle/3.3/src/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends BaseKernel
     {
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
-            if (isset($envs['all']) || isset($envs[$this->environment])) {
+            if ((isset($envs['all']) && $envs['all']) || (isset($envs[$this->environment]) && $envs[$this->environment])) {
                 yield new $class();
             }
         }
