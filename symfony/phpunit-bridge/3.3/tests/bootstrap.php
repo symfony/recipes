d49d21b71ee3d3c2c4b9077923da52a5262ceb5e
Ryan Weaver <ryan@thatsquality.com>
2018-02-11 09:54:36 -0500

    Adding a new test bootstrap.php file so .env vars are read
    This works particularly well, because any env vars set in
    phpunit.xml.dist WIN over .env. This means that .env can
    contain default values, but can then be easily overridden
    for the test environment.

---
+++ symfony/phpunit-bridge/3.3/tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+/*
+ * Environment variables can also be specified in phpunit.xml.dist.
+ * Those variables will override any defined in .env.
+ */
+if (!isset($_SERVER['APP_ENV'])) {
+    if (!class_exists(Dotenv::class)) {
+        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+    }
+    (new Dotenv())->load(__DIR__.'/../.env');
+}
+
+$debug = $_SERVER['APP_DEBUG'] ?? true;
+
+if ($debug) {
+    umask(0000);
+}
