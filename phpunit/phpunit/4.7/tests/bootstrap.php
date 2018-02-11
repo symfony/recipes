5d5d9e69679cbe157057d4a64bec574237f50fc5
Ryan Weaver <ryan@thatsquality.com>
2018-02-11 14:05:06 -0500

    Loading .env even if APP_ENV is set
    This is tough: we're getting WTF moments when someone tries to set,
    for example, APP_ENV=test or some other environment, and suddenly
    the .env file isn't loaded.
    
    This is to hopefully make things work in a more predictable way: .env
    is always loaded, except in the prod environment. This works because
    DotEnv does not override any existing variables, so there's no risk
    in loading it.

--- phpunit/phpunit/4.7/tests/bootstrap.php
+++ phpunit/phpunit/4.7/tests/bootstrap.php
@@ -8,7 +8,8 @@ use Symfony\Component\Dotenv\Dotenv;
  * Environment variables can also be specified in phpunit.xml.dist.
  * Those variables will override any defined in .env.
  */
-if (!isset($_SERVER['APP_ENV'])) {
+
+if (!isset($_SERVER['APP_ENV']) || 'prod' !== $_SERVER['APP_ENV']) {
     if (!class_exists(Dotenv::class)) {
         throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
     }
