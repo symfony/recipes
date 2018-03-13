e6907306bcd070f630f82f2d399b3d75ceb34a49
Ryan Weaver <ryan@thatsquality.com>
2018-03-13 13:59:33 -0400

    Add container tracking to bundles.php so the container rebuilds when it changes

--- symfony/framework-bundle/3.3/src/Kernel.php
+++ symfony/framework-bundle/3.3/src/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
     {
+        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
         // Feel free to remove the "container.autowiring.strict_mode" parameter
         // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
         $container->setParameter('container.autowiring.strict_mode', true);
