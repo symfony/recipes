b9e690bdfab02587450b318cca28b2caec1f6a58
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-05-23 15:23:52 +0200

    [webpack-encore] use versioned assets

--- symfony/webpack-encore-pack/1.0/webpack.config.js
+++ symfony/webpack-encore-pack/1.0/webpack.config.js
@@ -5,20 +5,27 @@ Encore
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+    // the path inside the Symfony app, as given to the asset() function
+    .setManifestKeyPrefix('build/')
+
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+
+    // the following line enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    //.addEntry('js/app', './assets/js/app.js')
+    //.addStyleEntry('css/app', './assets/css/app.scss')
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
 
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    //.enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    //.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
