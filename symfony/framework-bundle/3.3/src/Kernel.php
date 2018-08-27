6d7a9c58ff3cdedd8de2aa5ae3607f69ba8695e8
Alex Kalineskou <c.alex2005.mail@gmail.com>
2018-08-27 23:28:18 +0300

    [FrameworkBundle] Load parameters config in Kernel.php
    Load before packages, so an env parameters.yml can overwrite it

--- symfony/framework-bundle/3.3/src/Kernel.php
+++ symfony/framework-bundle/3.3/src/Kernel.php
@@ -44,6 +44,7 @@ class Kernel extends BaseKernel
         $container->setParameter('container.dumper.inline_class_loader', true);
         $confDir = $this->getProjectDir().'/config';
 
+        $loader->load($confDir.'/{parameters}'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
