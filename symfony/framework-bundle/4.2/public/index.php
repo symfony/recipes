92f13f6c972c6ee239746bb24308ce52d86c0150
Kévin Dunglas <dunglas@gmail.com>
2018-09-21 15:28:29 +0200

    Always call DotEnv if available

--- symfony/framework-bundle/4.2/public/index.php
+++ symfony/framework-bundle/4.2/public/index.php
@@ -2,18 +2,9 @@
 
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__.'/../vendor/autoload.php';
-
-// The check is to ensure we don't use .env if APP_ENV is defined
-if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-    }
-    (new Dotenv())->loadForEnv('dev', __DIR__.'/../.env');
-}
+require __DIR__.'/../src/bootstrap.php';
 
 $env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
 $debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
--- symfony/phpunit-bridge/4.2/tests/bootstrap.php
+++ symfony/phpunit-bridge/4.2/tests/bootstrap.php
@@ -4,10 +4,8 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\Dotenv\Dotenv;
 
-// The check is to ensure we don't use .env if APP_ENV is defined
-if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-    }
-    (new Dotenv())->loadForEnv('test', __DIR__.'/../.env');
+if (class_exists(Dotenv::class)) {
+    (new Dotenv())->loadForEnv($_SERVER['APP_ENV'] ?? 'test', __DIR__.'/../.env');
+} elseif (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
+    throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
 }
