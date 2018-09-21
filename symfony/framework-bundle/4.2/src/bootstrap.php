b590a560a0c21cc58a5036e086d63b5d8c73970e
Kévin Dunglas <dunglas@gmail.com>
2018-09-22 00:48:39 +0200

    Factorize bootstrap code

--- symfony/framework-bundle/4.2/src/bootstrap.php
+++ symfony/framework-bundle/4.2/src/bootstrap.php
@@ -5,7 +5,7 @@ require __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\Dotenv\Dotenv;
 
 if (class_exists(Dotenv::class)) {
-    (new Dotenv())->loadForEnv($_SERVER['APP_ENV'] ?? 'test', __DIR__.'/../.env');
+    (new Dotenv())->loadForEnv($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'test', __DIR__.'/../.env');
 } elseif (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
     throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
 }
