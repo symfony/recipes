f83edeee491bdce7e388c64693d5f6958595f96d
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-11-09 22:20:11 +0100

    fix namespace

--- symfony/framework-bundle/4.2/src/Kernel.php
+++ symfony/framework-bundle/4.2/src/Kernel.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
-use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
