f6585a3b41c32eef349c4aa51be705fcf98b97e6
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-11-17 07:21:00 +0100

    TRUSTED_HOSTS is a regexp

--- symfony/framework-bundle/3.3/public/index.php
+++ symfony/framework-bundle/3.3/public/index.php
@@ -17,7 +17,7 @@ if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ??
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts(explode(',', $trustedHosts));
+    Request::setTrustedHosts([$trustedHosts]);
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], $_SERVER['APP_DEBUG']);
