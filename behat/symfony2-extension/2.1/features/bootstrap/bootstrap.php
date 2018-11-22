f54d6a468405d0d8d27b0e790dc09a01e337777a
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-11-22 16:07:00 +0100

    Move src/.bootstrap.php to config/bootstrap.php

--- behat/symfony2-extension/2.1/features/bootstrap/bootstrap.php
+++ behat/symfony2-extension/2.1/features/bootstrap/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
 putenv('APP_ENV='.$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'test');
-require dirname(__DIR__, 2).'/src/.bootstrap.php';
+require dirname(__DIR__, 2).'/config/bootstrap.php';
--- symfony/framework-bundle/3.3/public/index.php
+++ symfony/framework-bundle/3.3/public/index.php
@@ -4,7 +4,7 @@ use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/src/.bootstrap.php';
+require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
