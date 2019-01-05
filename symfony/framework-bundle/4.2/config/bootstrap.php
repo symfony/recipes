8f35a294dc2ffecd9e53e2db12b57eb0a88c2200
Ryan Weaver <ryan@thatsquality.com>
2019-01-05 10:41:48 -0500

    Allowing APP_ENV to be an empty index

--- symfony/framework-bundle/4.2/config/bootstrap.php
+++ symfony/framework-bundle/4.2/config/bootstrap.php
@@ -16,6 +16,6 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
     (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
 }
 
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?: $_ENV['APP_ENV'] ?: 'dev';
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
