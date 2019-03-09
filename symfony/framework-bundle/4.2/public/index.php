97b5d06dfebf902ad68285899e176a4415fe1115
Daniel Chodusov <daniel@chodusov.com>
2019-03-09 12:41:16 +0800

    explode trustedHosts to array
    this fixes problem when Symfony\Component\HttpFoundation\Request::getHost doesn’t match pattern when hosts are separated by comma in default example .env file and throws untrusted host exception

--- symfony/framework-bundle/4.2/public/index.php
+++ symfony/framework-bundle/4.2/public/index.php
@@ -17,7 +17,7 @@ if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ??
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
+    Request::setTrustedHosts(explode(',', $trustedHosts));
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
