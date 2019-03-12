02769ee4a1927202cfe89c56fbaa00f9400e929a
Trent Steel <trsteel88@gmail.com>
2019-03-12 11:45:48 +1000

    Include test env for BC

--- symfony/framework-bundle/4.2/config/bootstrap.php
+++ symfony/framework-bundle/4.2/config/bootstrap.php
@@ -17,5 +17,5 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
 }
 
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
-$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'dev' === $_SERVER['APP_ENV'];
+$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'dev' === $_SERVER['APP_ENV'] || 'test' === $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
