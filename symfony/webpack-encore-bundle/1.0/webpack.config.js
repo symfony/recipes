e8b4f0689ca150ca7b786eb690aa98ef28b1a648
Lyrkan <850046+Lyrkan@users.noreply.github.com>
2019-03-27 14:16:28 +0100

    Encore: Make @babel/preset-env use polyfills by default

--- symfony/webpack-encore-bundle/1.0/webpack.config.js
+++ symfony/webpack-encore-bundle/1.0/webpack.config.js
@@ -41,6 +41,12 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
+    // enables @babel/preset-env polyfills
+    .configureBabel(() => {}, {
+        useBuiltIns: 'usage',
+        corejs: 3
+    })
+
     // enables Sass/SCSS support
     //.enableSassLoader()
 
