3c583b5ab3ee59bdc30565d7ab59020f516869ec
Hugo Alliaume <kocal@live.fr>
2019-06-01 07:30:08 +0200

    add a comment

--- symfony/webpack-encore-bundle/1.0/webpack.config.js
+++ symfony/webpack-encore-bundle/1.0/webpack.config.js
@@ -1,5 +1,7 @@
 var Encore = require('@symfony/webpack-encore');
 
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
