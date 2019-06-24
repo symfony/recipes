beac3f69a6f5cbd23f3e3812958ea49e8c06207c
Victor Bocharsky <bocharsky.bw@gmail.com>
2019-06-24 13:16:20 +0300

    Enable integrity hashes on production only by default

--- symfony/webpack-encore-bundle/1.0/webpack.config.js
+++ symfony/webpack-encore-bundle/1.0/webpack.config.js
@@ -61,7 +61,7 @@ Encore
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes()
+    //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
