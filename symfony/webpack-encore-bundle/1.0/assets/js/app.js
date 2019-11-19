5fe4ccf3b1bb577488395cf59cc00448c586fa66
Antoine Makdessi <antoine.makdessi@agriconomie.com>
2019-11-19 14:39:45 +0100

    Update app.js

--- symfony/webpack-encore-bundle/1.0/assets/js/app.js
+++ symfony/webpack-encore-bundle/1.0/assets/js/app.js
@@ -5,10 +5,10 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
