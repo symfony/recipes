0d9da5d1fff4e1b30344913b0e5f2865b581a3cf
TavoNiievez <64917965+TavoNiievez@users.noreply.github.com>
2020-11-05 07:47:26 -0500

    Allow PHP config in /packages and /routes folders

--- symfony/framework-bundle/5.1/src/Kernel.php
+++ symfony/framework-bundle/5.1/src/Kernel.php
@@ -13,7 +13,9 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
+        $container->import('../config/{packages}/*.php');
         $container->import('../config/{packages}/*.yaml');
+        $container->import('../config/{packages}/'.$this->environment.'/*.php');
         $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
@@ -26,7 +28,9 @@ class Kernel extends BaseKernel
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
+        $routes->import('../config/{routes}/'.$this->environment.'/*.php');
         $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../config/{routes}/*.php');
         $routes->import('../config/{routes}/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
