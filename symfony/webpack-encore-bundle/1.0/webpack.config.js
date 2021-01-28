89bbcccd7ff57bc0ee78222f699e61bd9632dd5d
Ryan Weaver <ryan@thatsquality.com>
2021-01-28 11:02:27 -0500

    switching var to const to be more modern

--- symfony/webpack-encore-bundle/1.0/webpack.config.js
+++ symfony/webpack-encore-bundle/1.0/webpack.config.js
@@ -1,4 +1,4 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
