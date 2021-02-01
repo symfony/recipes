774274dae00f8de20b021df90a7c7bcedc2d9827
Ryan Weaver <weaverryan+github@gmail.com>
2021-02-01 14:57:45 -0500

    fixing line break

--- symfony/webpack-encore-bundle/1.0/assets/bootstrap.js
+++ symfony/webpack-encore-bundle/1.0/assets/bootstrap.js
@@ -3,5 +3,6 @@ import { startStimulusApp } from '@symfony/stimulus-bridge';
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true, /\.(j|t)sx?$/
+    true,
+    /\.(j|t)sx?$/
 ));
