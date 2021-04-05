086f4079a9a25130e9dc2945abbf646583f17716
Zbigniew Malcherczyk <zmalcherczyk@gmail.com>
2021-04-05 12:48:02 +0200

    Load environment configured services and routes for php extension config
    Documentation states that application loads `config/services_<environment-name>.yaml (and services_<environment-name>.xml and services_<environment-name>.php files too).` https://symfony.com/doc/current/configuration.html, but for me, it looks like it does not auto load PHP configuration - I know that Symfony does no support XML configuration anymore, but as far as I know we would like to keep PHP config files.

--- symfony/framework-bundle/5.3/src/Kernel.php
+++ symfony/framework-bundle/5.3/src/Kernel.php
@@ -16,11 +16,15 @@ class Kernel extends BaseKernel
         $container->import('../config/{packages}/*.yaml');
         $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
+        if (\is_file(\dirname(__DIR__).'/config/services.yaml')) {
             $container->import('../config/services.yaml');
             $container->import('../config/{services}_'.$this->environment.'.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
+        } elseif (\is_file($path = \dirname(__DIR__).'/config/services.php')) {
             (require $path)($container->withPath($path), $this);
+            
+            if (\is_file($path = \dirname(__DIR__).'/config/services_'.$this->environment.'.php')) {
+                (require $path)($container->withPath($path), $this);
+            }
         }
     }
 
@@ -29,10 +33,14 @@ class Kernel extends BaseKernel
         $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
+        if (\is_file(\dirname(__DIR__).'/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
+        } elseif (\is_file($path = \dirname(__DIR__).'/config/routes.php')) {
             (require $path)($routes->withPath($path), $this);
+            
+            if (\is_file($path = \dirname(__DIR__).'/config/routes_'.$this->environment.'.php')) {
+                (require $path)($container->withPath($path), $this);
+            }
         }
     }
 }
