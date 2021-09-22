7c81e7e9e16930af309f79729e86991b91644028
Marko Kaznovac <kaznovac@gmail.com>
2021-09-22 23:25:45 +0200

    pass static functions to runtime (clearer intent and improves performance)

--- symfony/framework-bundle/5.4/public/index.php
+++ symfony/framework-bundle/5.4/public/index.php
@@ -4,6 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
+return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
