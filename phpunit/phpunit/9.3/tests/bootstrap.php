1e15fa94edd1def3ec3db4c67843890816b70fea
Roland Franssen :) <franssen.roland@gmail.com>
2022-02-12 14:35:32 +0100

    Update bootstrap.php

--- phpunit/phpunit/9.3/tests/bootstrap.php
+++ phpunit/phpunit/9.3/tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
@@ -8,4 +9,8 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+
+if ($_SERVER['APP_DEBUG'] && class_exists(Debug::class)) {
+    Debug::enable();
 }
