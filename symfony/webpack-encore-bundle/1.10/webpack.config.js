fb0ee4415c52728d024e78b715b9206224ac03e6
Arnaud Lier <arnaud@eclixo.com>
2022-06-19 16:25:25 +0200

    Fix small typo in symfony/webpack-encore-bundle

--- symfony/webpack-encore-bundle/1.10/webpack.config.js
+++ symfony/webpack-encore-bundle/1.10/webpack.config.js
@@ -11,7 +11,7 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
+    // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
     /*
