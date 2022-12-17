8a868a8ae6c506eb4cccd62e5eb886166b581de4
Alexander Schranz <alexander@sulu.io>
2022-12-17 22:27:18 +0100

    CI not allow symlink so we make a copy

--- php-cs-fixer/shim/3.0/.php-cs-fixer.dist.php
+++ php-cs-fixer/shim/3.0/.php-cs-fixer.dist.php
@@ -1,1 +1,13 @@
-../../../friendsofphp/php-cs-fixer/3.0/.php-cs-fixer.dist.php
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+;
