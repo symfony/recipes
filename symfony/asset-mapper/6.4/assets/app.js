b0de8d27160ef1690049b32d77c26aeb3bb80809
Tac Tacelosky <tacman@gmail.com>
2023-10-09 19:05:11 -0400

    fix paths and add entrypoint comment

--- symfony/asset-mapper/6.4/assets/app.js
+++ symfony/asset-mapper/6.4/assets/app.js
@@ -4,4 +4,6 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './styles/app.css'
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
