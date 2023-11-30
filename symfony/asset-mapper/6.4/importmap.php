d27fe1b2160f09be15c4c2aee15f85ffc6c60d91
Marko Kaznovac <kaznovac@users.noreply.github.com>
2023-11-30 15:34:28 +0100

    asset-mapper: fix minor whitespace inconsistency in importmap.php

--- symfony/asset-mapper/6.4/importmap.php
+++ symfony/asset-mapper/6.4/importmap.php
@@ -6,8 +6,8 @@
  * - "path" is a path inside the asset mapper system. Use the
  *     "debug:asset-map" command to see the full list of paths.
  *
- *  - "entrypoint" (JavaScript only) set to true for any module that will
- *      be used as an "entrypoint" (and passed to the importmap() Twig function).
+ * - "entrypoint" (JavaScript only) set to true for any module that will
+ *     be used as an "entrypoint" (and passed to the importmap() Twig function).
  *
  * The "importmap:require" command can be used to add new entries to this file.
  *
