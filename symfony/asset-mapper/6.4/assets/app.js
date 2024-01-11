0ff0a0af1cc9e7e278597e04bc6010e27d6d0498
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 01:35:04 -0500

    use semicolons in JavaScript (#1252)

--- symfony/asset-mapper/6.4/assets/app.js
+++ symfony/asset-mapper/6.4/assets/app.js
@@ -4,6 +4,6 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css'
+import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
