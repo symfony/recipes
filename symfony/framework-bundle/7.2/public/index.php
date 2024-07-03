ed28e4c8e63f65a78153a5d17e2e34e8878703cb
Antoine M <amakdessi@me.com>
2024-07-03 10:32:23 +0200

    Check for existence of vendor directory for better error message
    replicates https://github.com/symfony/recipes/pull/1301 for the web entry point

--- symfony/framework-bundle/7.2/public/index.php
+++ symfony/framework-bundle/7.2/public/index.php
@@ -2,6 +2,14 @@
 
 use App\Kernel;
 
+if (!is_dir(dirname(__DIR__).'/vendor')) {
+    throw new LogicException('Dependencies are missing. Try running "composer install".');
+}
+
+if (!is_file(dirname(__DIR__).'/vendor/autoload_runtime.php')) {
+    throw new LogicException('Symfony Runtime is missing. Try running "composer require symfony/runtime".');
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
