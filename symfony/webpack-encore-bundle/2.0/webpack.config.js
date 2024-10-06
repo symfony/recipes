d62024a8a5f54a5b9dac91488ed700afa98442fb
Hugo Alliaume <hugo@alliau.me>
2024-10-06 11:34:46 +0200

    [WebpackEncoreBundle] Update recipe for Webpack Encore 5 (#1341)

--- symfony/webpack-encore-bundle/2.0/webpack.config.js
+++ symfony/webpack-encore-bundle/2.0/webpack.config.js
@@ -50,7 +50,7 @@ Encore
     // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
+        config.corejs = '3.38';
     })
 
     // enables Sass/SCSS support
