2ab03d62cc7450bf548cef4ec7068471d2b5bf32
Wouter de Jong <wouterj@users.noreply.github.com>
2025-04-14 11:49:29 +0200

    Make PHPUnit 10 config more consistent with PHPUnitBridge behavior (#1402)
    * Make PHPUnit 10 config more consistent with PHPUnitBridge behavior
    
    * Set umask(1000) when debug is true
    
    Applies https://github.com/symfony/recipes/pull/889 to the PHPUnit
    recipe.

--- phpunit/phpunit/10.0/tests/bootstrap.php
+++ phpunit/phpunit/10.0/tests/bootstrap.php
@@ -7,3 +7,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+}
