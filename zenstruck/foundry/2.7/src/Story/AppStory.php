7e7747e2adc6b39c7fa388dd6d81918922ba69bb
Nicolas PHILIPPE <nikophil@gmail.com>
2025-07-28 15:55:20 +0200

    [zenstruck/foundry] enable auto refresh with lazy objects in 2.7 (#1441)

---
+++ zenstruck/foundry/2.7/src/Story/AppStory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Story;
+
+use Zenstruck\Foundry\Attribute\AsFixture;
+use Zenstruck\Foundry\Story;
+
+#[AsFixture(name: 'main')]
+final class AppStory extends Story
+{
+    public function build(): void
+    {
+        // SomeFactory::createOne();
+    }
+}
