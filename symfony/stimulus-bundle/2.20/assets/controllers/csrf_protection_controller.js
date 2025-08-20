dd489df577b64c869bb54a3e7ebc8651329bf7d5
Marcos Gómez Vilches <marcosmgv@gmail.com>
2025-08-20 11:54:48 +0200

    fix: [LiveComponent] CSRF token not valid after form second submit
    Fix the “invalid CSRF token” error when working with live-components and submitting a form a second time, for example if a server-side validation fails and the form is re-rendered.

--- symfony/stimulus-bundle/2.20/assets/controllers/csrf_protection_controller.js
+++ symfony/stimulus-bundle/2.20/assets/controllers/csrf_protection_controller.js
@@ -33,8 +33,8 @@ export function generateCsrfToken (formElement) {
     if (!csrfCookie && nameCheck.test(csrfToken)) {
         csrfField.setAttribute('data-csrf-protection-cookie-value', csrfCookie = csrfToken);
         csrfField.defaultValue = csrfToken = btoa(String.fromCharCode.apply(null, (window.crypto || window.msCrypto).getRandomValues(new Uint8Array(18))));
-        csrfField.dispatchEvent(new Event('change', { bubbles: true }));
     }
+    csrfField.dispatchEvent(new Event('change', { bubbles: true }));
 
     if (csrfCookie && tokenCheck.test(csrfToken)) {
         const cookie = csrfCookie + '_' + csrfToken + '=' + csrfCookie + '; path=/; samesite=strict';
