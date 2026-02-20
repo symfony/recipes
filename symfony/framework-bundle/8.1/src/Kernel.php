07c59321715367caaf2dd2f20516e6882cfcda3f
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-02-20 10:53:10 +0100

    [symfony/framework-bundle/8.1] Add Kernel::getAllowedEnvs() = prod+dev+test

---
+++ symfony/framework-bundle/8.1/src/Kernel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    /**
+     * @return list<string> An array of allowed values for APP_ENV
+     */
+    private function getAllowedEnvs(): array
+    {
+        return ['prod', 'dev', 'test'];
+    }
+}
